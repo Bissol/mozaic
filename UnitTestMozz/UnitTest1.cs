c8ee30cf3e57e09b2cf6bd53afa3654636e83943
Bissol <stephane.bissol@gmail.com>
2016-12-01 18:12:19 +0100

    ...

--- UnitTestMozz/UnitTest1.cs
+++ UnitTestMozz/UnitTest1.cs
@@ -12,8 +12,32 @@ namespace UnitTestMozz
         public void TestMethod1()
         {
             Bitmap bmp = UnitTestMozz.Properties.Resources.test;
-            ImageProcessing.MergeEdgesVertical(ref bmp, 150, 30);
-            bmp.Save("test1.png");
+            ImageProcessing.MergeEdgesVertical(ref bmp, 150, 10);
+            bmp.Save("testBasic.png");
+        }
+
+        [TestMethod]
+        public void TestSmartEdge()
+        {
+            Bitmap bmp = UnitTestMozz.Properties.Resources.test;
+            ImageProcessing.SmartMergeVertical(ref bmp, 150, 40, 5, 5, 15f);
+            bmp.Save("testSmart.png");
+        }
+
+        [TestMethod]
+        public void TestAvgAvg()
+        {
+            Bitmap bmp = UnitTestMozz.Properties.Resources.test;
+            ImageProcessing.test(ref bmp, 5, 5, false);
+            bmp.Save("testavg.png");
+        }
+
+        [TestMethod]
+        public void TestAvgSmoothness()
+        {
+            Bitmap bmp = UnitTestMozz.Properties.Resources.test;
+            ImageProcessing.test(ref bmp, 5, 5, true);
+            bmp.Save("testsmooth.png");
         }
     }
 }
--- mozaic/ImageProcessing.cs
+++ mozaic/ImageProcessing.cs
@@ -144,7 +144,8 @@ namespace mozaic
             imageAttributes.SetGamma(gamma, ColorAdjustType.Bitmap);
         }
 
-        public static void MergeEdgesVertical(ref Bitmap bmp, int xpos, int mergeSize)
+        // Smart edges (tm) ;-)
+        public static void SmartMergeVertical(ref Bitmap bmp, int xpos, int mergeSize, int smoothness, int avgMapSize, float stopThreshold)
         {
             BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
             int stride = data.Stride;
@@ -162,12 +163,81 @@ namespace mozaic
 
                 // Compute average
                 float[] averageMap = new float[length];
-                ImageProcessing.computeAvgMap(ref averageMap, stride, bppModifier, bmp.Width, bmp.Height, copy, 5);
+                ImageProcessing.computeAvgMap(ref averageMap, stride, bppModifier, bmp.Width, bmp.Height, copy, avgMapSize);
 
                 // Get image smoothness
-                float[] smoothness = new float[length];
-                ImageProcessing.computeSmoothnessMap(ref smoothness, stride, bppModifier, bmp.Width, bmp.Height, copy, 5);
+                float[] smoothnessMap = new float[length];
+                ImageProcessing.computeSmoothnessMap(ref smoothnessMap, averageMap, stride, bppModifier, bmp.Width, bmp.Height, copy, smoothness);
 
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    // Decide on a direction (left or right)
+                    bool dirIsRight = false;
+                    float smoothleft = smoothnessMap[y * bmp.Width + (xpos - smoothness)];
+                    float smoothright = smoothnessMap[y * bmp.Width + (xpos + smoothness)];
+                    dirIsRight = smoothleft < smoothright;
+
+                    // Determine a 'stop'
+                    use avg?
+                        do it both ways
+                    int stopx = xpos;
+                    for (int sx = xpos + smoothness; sx < xpos + mergeSize; sx++)
+                    {
+                        if (smoothnessMap[y * bmp.Width + sx] > stopThreshold)
+                        {
+                            stopx = sx;
+                            break;
+                        }
+                    }
+
+                    // How many smoothing pixels?
+                    int nbPixels = Math.Abs(stopx - xpos);
+
+                    // Make border
+                    for (int x = xpos; x != stopx; x += (dirIsRight ? 1 : -1))
+                    {
+                        // Get x,y color
+                        int idx = (y * stride) + x * bppModifier;
+                        red = ptr[idx + 2];
+                        green = ptr[idx + 1];
+                        blue = ptr[idx];
+
+                        // Get merge color (symetry)
+                        int symidx = (y * stride) + (x > xpos ? (xpos - (x - xpos)) : xpos + (xpos - x)) * bppModifier;
+                        red2 = ptr[symidx + 2];
+                        green2 = ptr[symidx + 1];
+                        blue2 = ptr[symidx];
+
+                        // Get merge factor f(x)
+                        float localPixWeight = (float)Math.Abs((float)x - (float)xpos) / (float)nbPixels;
+                        //if (x > xpos) localPixWeight = 1;// 1.0f / localPixWeight;
+                        float symPixWeight = 1.0F - localPixWeight;
+
+                        // Merge
+                        red = (int)(localPixWeight * (float)red + symPixWeight * (float)red2);
+                        green = (int)(localPixWeight * (float)green + symPixWeight * (float)green2);
+                        blue = (int)(localPixWeight * (float)blue + symPixWeight * (float)blue2);
+
+                        // Write result
+                        ptr[(x * bppModifier) + y * stride] = (byte)blue;
+                        ptr[(x * bppModifier) + y * stride + 1] = (byte)green;
+                        ptr[(x * bppModifier) + y * stride + 2] = (byte)red;
+                    }
+                }
+            }
+        }
+
+        public static void MergeEdgesVertical(ref Bitmap bmp, int xpos, int mergeSize)
+        {
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+            int stride = data.Stride;
+            int bppModifier = bmp.PixelFormat == System.Drawing.Imaging.PixelFormat.Format24bppRgb ? 3 : 4;
+            int red, green, blue = 0;
+            int red2, green2, blue2 = 0;
+            unsafe
+            {
+                byte* ptr = (byte*)data.Scan0;
+                
                 for (int y = 0; y < bmp.Height; y++)
                 {
                     for (int x = xpos - mergeSize; x < xpos + mergeSize; x++)
@@ -179,14 +249,14 @@ namespace mozaic
                         blue = ptr[idx];
 
                         // Get merge color (symetry)
-                        int symidx = (y * stride) + (x>mergeSize ? (mergeSize - (x - mergeSize)) : mergeSize+(mergeSize-x)) * bppModifier;
+                        int symidx = (y * stride) + (x>xpos ? (xpos - (x - xpos)) : xpos+(xpos-x)) * bppModifier;
                         red2 = ptr[symidx + 2];
                         green2 = ptr[symidx + 1];
                         blue2 = ptr[symidx];
 
                         // Get merge factor f(x)
-                        float localPixWeight =  Math.Abs((float)x - (float)mergeSize) / (float)mergeSize;
-                        if (x > mergeSize) localPixWeight = 1;// 1.0f / localPixWeight;
+                        float localPixWeight =  Math.Abs((float)x - (float)xpos) / (float)mergeSize;
+                        if (x > xpos) localPixWeight = 1;// 1.0f / localPixWeight;
                         float symPixWeight = 1.0F - localPixWeight;
 
                         // Merge
@@ -194,7 +264,6 @@ namespace mozaic
                         green = (int)(localPixWeight * (float)green + symPixWeight * (float)green2);
                         blue = (int)(localPixWeight * (float)blue + symPixWeight * (float)blue2);
 
-                        //red = (int)averageMap[y * bmp.Width + x];
                         // Write result
                         ptr[(x * bppModifier) + y * stride] = (byte)blue;
                         ptr[(x * bppModifier) + y * stride + 1] = (byte)green;
@@ -237,13 +306,76 @@ namespace mozaic
             return intensity / count;
         }
 
-        private static void computeSmoothnessMap(ref float[] map, int stride, int bppModifier, int width, int height, byte[] src, int matrixHalfSize)
+        private static void computeSmoothnessMap(ref float[] map, float[] avgs, int stride, int bppModifier, int width, int height, byte[] src, int matrixHalfSize)
         {
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
                     // Get smoothness for pixel x,y
+                    float avg = avgs[y * width + x];
+                    float smt = getSmoothnessForPixel(x, y, avg, stride, bppModifier, width, height, src, matrixHalfSize);
+                    map[y * width + x] = smt;
+                }
+            }
+        }
+
+        private static float getSmoothnessForPixel(int x0, int y0, float avg, int stride, int bppModifier, int width, int height, byte[] src, int matrixHalfSize)
+        {
+            float smoothness = 0f;
+            int count = 0;
+            for (int y = y0 - matrixHalfSize; y < y0 + matrixHalfSize; y++)
+            {
+                for (int x = x0 - matrixHalfSize; x < x0 + matrixHalfSize; x++)
+                {
+                    if (x < 0 || x >= width || y < 0 || y >= height) continue;
+                    count++;
+
+                    // Get x,y color
+                    int idx = (y * stride) + x * bppModifier;
+                    float localIntensity = (src[idx + 2] + src[idx + 1] + src[idx]) / 3;
+                    smoothness += Math.Abs(localIntensity - avg);
+                }
+            }
+
+            return smoothness / count;
+        }
+
+        public static void test(ref Bitmap bmp, int avgMapSize, int smoothness, bool showSmoothness)
+        {
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+            int stride = data.Stride;
+            int bppModifier = bmp.PixelFormat == System.Drawing.Imaging.PixelFormat.Format24bppRgb ? 3 : 4;
+            int red, green, blue = 0;
+
+            // Make a copy
+            int length = Math.Abs(data.Stride) * data.Height;
+            byte[] copy = new byte[length];
+            Marshal.Copy(data.Scan0, copy, 0, copy.Length);
+
+            // Compute average
+            float[] averageMap = new float[length];
+            ImageProcessing.computeAvgMap(ref averageMap, stride, bppModifier, bmp.Width, bmp.Height, copy, avgMapSize);
+
+            // Get image smoothness
+            float[] smoothnessMap = new float[length];
+            ImageProcessing.computeSmoothnessMap(ref smoothnessMap, averageMap, stride, bppModifier, bmp.Width, bmp.Height, copy, smoothness);
+
+            unsafe
+            {
+                byte* ptr = (byte*)data.Scan0;
+
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        int val = showSmoothness ? (int)smoothnessMap[y * bmp.Width + x] :(int)averageMap[y * bmp.Width + x];
+
+                        // Write result
+                        ptr[(x * bppModifier) + y * stride] = (byte)val;
+                        ptr[(x * bppModifier) + y * stride + 1] = (byte)val;
+                        ptr[(x * bppModifier) + y * stride + 2] = (byte)val;
+                    }
                 }
             }
         }
